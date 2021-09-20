2deceb6c204e061b809ac0fce58b43229f2e46f3
Nwigwe Uzochukwu <uzochukwunwigwe@gmail.com>
2021-09-20 12:43:40 +0100

    Updated the swagger doc for the department/createDepartment route

--- server/routes/department.js
+++ server/routes/department.js
@@ -3,6 +3,64 @@ const { createDepartment } = require("../controller/department");
 
 // Create Department
 // Swagger Definition
+/**
+ * @swagger
+ * /department/createDepartment:
+ *   post:
+ *     description: Create an employee
+ *     parameters:
+ *       - name: Request body
+ *         in: body
+ *         description: Request body object
+ *         schema:
+ *           type: object
+ *           required: 
+ *             - name
+ *           properties:
+ *             name: 
+ *               type: string
+ *     responses: 
+ *       200:
+ *         description: Response body object
+ *         schema: 
+ *           type: object  
+ *           properties: 
+ *             message:
+ *               type: string
+ *             success: 
+ *               type: boolean
+ *       422:
+ *          description: Response body object
+ *          schema: 
+ *            type: object  
+ *            properties: 
+ *              message:
+ *                type: string
+ *              success: 
+ *                type: boolean
+ *                enum: [false, true]
+ *       500:
+ *          description: Response body object
+ *          schema: 
+ *            type: object  
+ *            properties: 
+ *              message:
+ *                type: string
+ *              success: 
+ *                type: boolean
+ *                enum: [false, true]
+ *       501:
+ *          description: Not implemented
+ *          schema: 
+ *            type: object  
+ *            properties: 
+ *              message:
+ *                type: string
+ *              success: 
+ *                type: boolean
+ *                enum: [false, true] 
+ *             
+ */
 router.post('/createDepartment', createDepartment);
 
 
